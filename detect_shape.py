e77ba565269daeb0814727e8fb5dde85d0b7eca7
Disha31294 <51826328+Disha31294@users.noreply.github.com>
2019-08-12 14:06:43 +0530

    Add files via upload

---
+++ detect_shape.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+font = cv2.FONT_HERSHEY_DUPLEX
+
+img = cv2.imread("test.jpg", cv2.IMREAD_GRAYSCALE)
+_, threshold = cv2.threshold(img, 240, 255, cv2.THRESH_BINARY)
+contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+for cnt in contours:
+    approx = cv2.approxPolyDP(cnt, 0.01*cv2.arcLength(cnt, True), True)
+    cv2.drawContours(img, [approx], 0, (0), 5)
+    x = approx.ravel()[0]
+    y = approx.ravel()[1]
+    if len(approx) == 4:
+        cv2.putText(img, "Square", (x, y), font, 1, (0))
+    elif len(approx) > 8:
+        cv2.putText(img, "Circle", (x, y), font, 1, (0))
+cv2.imshow("test", img)
+cv2.imshow("Threshold", threshold)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
